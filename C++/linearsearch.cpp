c552490df1419d7d5debb2887b3a28a0595c3b99
anuj544 <74566873+anuj544@users.noreply.github.com>
2021-10-04 16:04:37 +0530

    Final updation on linearsearch.cpp

--- C++/linearsearch.cpp
+++ C++/linearsearch.cpp
@@ -4,28 +4,30 @@ Author: Anuj Singh
 Date Modified: 4th October 2021
 */
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-  int n;
-  cin>>n;
-  int a[n];
-  int ans; 
-  for(int i=0;i<n;i++){
-    cin>>a[i];
-   }
-  int fn;
-  cout<<"enter a number to search"<<endl;
-  cin>>fn;
-  for(int i=0;i<n;i++){
-    if(a[i]==fn){
-      ans=i;
-      break;
-       }
+    int n;
+    cin >> n;
+    int a[n];
+    int ans;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
-    cout<<ans<<endl;
+    int fn;
+    cout << "enter a number to search" << endl;
+    cin >> fn;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == fn)
+        {
+            ans = i;
+            break;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
       
