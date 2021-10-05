470e8847020c946de1f7ebbfb87a56f5f98baf18
Shantanu <shansharma1301@gmail.com>
2021-10-05 19:59:29 +0530

    Delete Improper commits'

--- Python/Patient Problem.py
+++
@@ -1,7 +0,0 @@
-#How long will the last patient have to wait foe its turn. If doctore given 10 minute to every patient only. Every patient arrives at time X as input by the user.
-#Authur:Shantanu Sharma
-#Date Modified:4th October 2021
-for i in range(int(input("Enter the number of test cases : "))):
-    p=int(input("Enter number of patients: "))
-    X=int(input("Time at which every patient arrives: "))
-    print("The last patient had to wait for {} minutes".format(((p-1)*10)-((p-1)*X)))
