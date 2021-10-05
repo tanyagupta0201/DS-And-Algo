cdf5e5fade02bc6f04424f3a316910c943d45fad
devika6001 <87614573+devika6001@users.noreply.github.com>
2021-10-05 20:47:03 +0530

    write a program for reversing a string

--- C++/Reverse a String.cpp
+++ C++/Reverse a String.cpp
@@ -1,31 +1,32 @@
 /*
 Program to reverse a string
-Author: Laasya Gupta
-Date Modified: 4th October 2021
+Author: Devika Gupta
+Date Modified: 5th October 2021
 */
-
-
-#include <iostream>
+#include<iostream>
+#include<string.h>
 using namespace std;
-
-class Solution
+void reverse_string(char str[20])
 {
-public:
-    void reverseString(vector<char> &s)
+    char ch;
+    int l,n;
+    l=strlen(str);
+    n=l;
+    n--;
+    for(int i=0;i<=l/2;i++)
     {
-        int right = s.size() - 1;
-        int left = 0;
-
-        while (left < right)
-        {
-            char c = s[left];
-            s[left] = s[right];
-            s[right] = c;
-
-            left += 1;
-            right += 1;
-        }
+        ch=str[i];
+        str[i]=str[n];
+        str[n]=ch;
+        n--;
     }
-};
-
+    cout<<"Reversed string:"<<str;
+}
+        
+int main()
+{
+    char str[20]="Hacktoberfest";
+    reverse_string(str);
+    return 0;
+}
 
