5757edec87a887662f31ba27be6ceff5a5325881
shradhaverma001 <shradhaverma001@gmail.com>
2021-10-08 20:52:05 +0530

    Insertion_in_LL

---
+++ C++/Insertion_in_Linked_list.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+//  If we wish to change the value of a variable by a function, we pass pointer to it. 
+// And if we wish to change value of a pointer (i. e., it should start pointing to something else), we pass pointer to a pointer.
+class Node
+{
+    public:
+    int data;
+    Node* next;
+};
+void InsertBeg(Node** head, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void InsertAfter(Node* prevnode, int new_data)
+{
+    if (prevnode == NULL){
+        cout << "The given previous node cannot be null";
+        return;
+    }
+    Node* new_node = new Node();
+    new_node-> data = new_data;
+    new_node->next = prevnode->next;
+    prevnode->next = new_node;
+}
+
+void InsertEnd(Node** head, int new_data)
+{
+    Node* new_node = new Node();
+    Node* last = *head;
+    new_node->data = new_data;
+    new_node->next = NULL;
+    
+    if (*head == NULL){
+        *head = new_node;
+        return;
+    }
+    
+    while(last->next != NULL){
+        last = last->next;
+    }
+    
+    last->next = new_node;
+    return;
+}
+void printList(Node* node)
+{
+    while(node!= NULL)
+    {
+        cout << node->data << "->";
+        node = node->next;
+    }
+}
+int main()
+{
+    
+    Node* head = NULL;
+    InsertEnd(&head, 9);
+    InsertBeg(&head, 4);
+    InsertBeg(&head, 6);
+    InsertEnd(&head, 3);
+    InsertBeg(&head, 2);
+    InsertAfter(head->next,8);
+    cout << "Linked List created is ";
+    printList(head);
+    return 0;
+}
