18a9122ab69b2d030982b4858c58b070e34143c7
Shantanu <shansharma1301@gmail.com>
2021-10-08 21:33:41 +0530

    GCD

---
+++ python/Greatest_Common_Divisor.py
@@ -0,0 +1,10 @@
+def gcd (x, y):  
+    if (y == 0):   
+        return x   
+    else:  
+        return gcd (y, x % y)  
+x =int (input ("Enter the first number: "))    
+y =int (input ("Enter the second number: "))    
+num = gcd(x, y)  
+print("GCD of two number is: ")  
+print(num)  
