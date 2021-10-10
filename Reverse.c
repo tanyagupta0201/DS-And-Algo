4a5ec9be1e22afee39e62880bfb6f77671d5a190
pinto541 <78383405+pinto541@users.noreply.github.com>
2021-10-11 00:41:59 +0530

    Reverse.c
    Added reverse number program in c.

---
+++ Reverse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main{
+  int num,rev=0,rem;
+  printf("enter a number:");
+  scanf("%d"&num);
+  while(num>=1){
+    rem=num%10;
+    rev=rev*10+rem;
+    num=num/10;
+  }
+  printf("the reverse of %d is:%d",num,rev);
+  return 0;
+}
