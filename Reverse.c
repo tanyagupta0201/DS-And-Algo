5ca0c6916af97b1c2ec2ae728c38c2ee148987c3
pinto541 <78383405+pinto541@users.noreply.github.com>
2021-10-11 22:29:19 +0530

    Update Reverse.c

--- Reverse.c
+++ Reverse.c
@@ -4,15 +4,18 @@ Author's name-pinto kumar
 Data modified-11/10/2021
 */
 #include<stdio.h>
-int main{
-  int num,rev=0,rem;
-  printf("enter a number:");
-  scanf("%d"&num);
-  while(num>=1){
-    rem=num%10;
-    rev=rev*10+rem;
-    num=num/10;
-  }
-  printf("the reverse of %d is:%d",num,rev);
-  return 0;
+int main()
+{
+         int num,rev=0,rem;
+         printf("enter a number:");
+         scanf("%d"&num);
+  
+         while(num>=1){
+                      rem=num%10;
+                      rev=rev*10+rem;
+                      num=num/10;
+                      }
+  
+        printf("the reverse of %d is:%d",num,rev);
+        return 0;
 }
