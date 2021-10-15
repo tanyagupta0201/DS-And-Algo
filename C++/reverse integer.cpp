4a57cffcf5559c47bf6adb8bf5d091e04c4b2a47
priyanhsukashyap2601 <priyanhsu.kashyap2601@gmail.com>
2021-10-15 23:37:26 +0530

    Reverse an integer

---
+++ C++/reverse integer.cpp
@@ -0,0 +1,23 @@
+//Priyanshu Kashyap
+//15/10/2021
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int reversDigits(int num)
+{
+    int rev_num = 0;
+    while (num > 0) {
+        rev_num = rev_num * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev_num;
+}
+
+int main()
+{
+    int num = 12345678;
+    cout << "Reverse of no. is " << reversDigits(num);
+    getchar();
+    return 0;
+}
