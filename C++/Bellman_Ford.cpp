1ee57aae874887b1369dee4749cd28fcdda5c267
Neetu Chaudhary <91332294+neetu-749@users.noreply.github.com>
2021-10-20 11:57:54 +0530

    Bellman Ford Algorithm

---
+++ C++/Bellman_Ford.cpp
@@ -0,0 +1,50 @@
+/*
+Bellman Ford algorithm in cpp 
+Author:Neetu Kumari
+Date Modified:20-10-2021
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int infinity = 1e9;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector < vector < int > > edges;
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    edges.push_back( { u, v, w } );
+  }
+  int source;
+  cin >> source;
+  vector < int > dist(n, infinity);
+  dist[source] = 0;
+  for (int i = 0; i < n - 1; i++) {
+    for (auto e: edges) {
+      int u = e[0];
+      int v = e[1];
+      int w = e[2];
+      dist[v] = min(dist[v], w + dist[u]);
+    }
+  }
+  for (auto i: dist)
+    cout << i << " " << endl;
+}
+
+
+/* sample input for the graph
+5 8
+1 2 5
+3 2 4
+1 3 6
+3 1 1 
+1 4 1
+0 2 3
+4 3 -4
+0 1 -2
+0
+Output: 0 -6 -3 -9 -5
+*/
