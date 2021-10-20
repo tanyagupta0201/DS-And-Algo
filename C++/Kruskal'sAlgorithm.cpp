fa2171930633394a8e9a544eb2b77fb89e2cca56
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2021-10-20 06:13:02 +0530

    Modified Kruskal's Algorithm

--- C++/Kruskal'sAlgorithm.cpp
+++ C++/Kruskal'sAlgorithm.cpp
@@ -1,74 +1,76 @@
-/* Kruskal's algorithm on cpp
+/*
+Kruskal's algorithm on cpp
 Author:Neetu Kumari
 Date modified:19-10-2020
 */
 
-
-#include <iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
 
-const int n=1e5+6;
-vector<int> parent(n);
-vector<int> sz(n);
+const int n = 1e5 + 6;
+vector < int > parent(n);
+vector < int > sz(n);
 
-void makeSet(int v){
-    parent[v]=v;
-    sz[v]=1;
+void makeSet(int v) {
+  parent[v] = v;
+  sz[v] = 1;
 }
-int findSet(int v){
-    if(v==parent[v])
+int findSet(int v) {
+  if (v == parent[v])
     return v;
-    return parent[v]=findSet(parent[v]);
+  return parent[v] = findSet(parent[v]);
 }
-void unionSet(int a, int b){
-    a=findSet(a);
-    b=findSet(b);
-    if(a != b){
-        if(sz[a]<sz[b])
-        swap(a,b);
-        parent[b]=a;
-        sz[a] += sz[b];
-    }
+void unionSet(int a, int b) {
+  a = findSet(a);
+  b = findSet(b);
+  if (a != b) {
+    if (sz[a] < sz[b])
+      swap(a, b);
+    parent[b] = a;
+    sz[a] += sz[b];
+  }
 }
 
-int main()
-{
-    for(int i=0;i<n;i++){
-        makeSet(i);
+int main() {
+  for (int i = 0; i < n; i++) {
+    makeSet(i);
+  }
+  //x is no of vertices and y is no of edges
+  int x, y;
+  cout << "no of vertices:";
+  cin >> x;
+  cout << "no of edges:";
+  cin >> y;
+
+  vector < vector < int >> edges;
+  for (int i = 0; i < y; i++) {
+    int u, v, w; //w is weight of edge
+    cout << "starting vertex:" << " end vertex:" << " weight of edge:" << endl;
+    cin >> u >> v >> w;
+
+    edges.push_back({
+      w,
+      u,
+      v
+    });
+  }
+  //sort edges according to increasing order of their weights
+  sort(edges.begin(), edges.end());
+  int cost = 0; //cost=sum of weight of all edges in the tree
+  for (auto i: edges) {
+    int w = i[0];
+    int u = i[1];
+    int v = i[2];
+    int s = findSet(u);
+    int t = findSet(v);
+    if (s == t) {
+      continue;
+    } else {
+      cout << u << " " << v << endl;
+      cost += w; //add weight to cost
+      unionSet(u, v);
     }
-    //x is no of vertices and y is no of edges
-    int x,y;     
-    cout<<"no of vertices:";
-    cin>>x;
-    cout<<"no of edges:";
-    cin>>y;
-    
-    vector<vector<int>> edges;
-    for(int i=0;i<y;i++){
-        int u,v,w;         //w is weight of edge
-        cout<<"starting vertex:"<<" end vertex:"<<" weight of edge:"<<endl;
-        cin>>u>>v>>w;
-        
-        edges.push_back({w,u,v});
-    }
-    //sort edges according to increasing order of their weights
-    sort(edges.begin(),edges.end());
-    int cost=0;             //cost=sum of weight of all edges in the tree
-    for(auto i: edges){
-        int w=i[0];
-        int u=i[1];
-        int v=i[2];
-        int s=findSet(u);
-        int t=findSet(v);
-        if(s==t){
-            continue;
-        }
-        else{
-            cout<<u<<" "<<v<<endl;
-            cost += w;       //add weight to cost
-            unionSet(u,v);
-        }
-    }
-    cout<<cost;
+  }
+  cout << cost;
 }
