a1d067838251a4d1dceca62e57757bef1690dd3b
Ritz2626 <ritikarastogi2626@gmail.com>
2021-10-23 18:40:22 +0530

    Added a program of Bubble Sort in Java Language
    Added bubble sort Program

--- JAVA/bubble_sort.java
+++ JAVA/bubble_sort.java
@@ -1,39 +1,43 @@
+/*A program to implement bubble sort in java*/
+/*Author's Name:- Ritika Rastogi
+ *Date of Contribution-23-10-2021
+ */
 import java.util.*;
-class bubble_sort
+class bubble_sort 
 {
-    public static void bubbleSort(int a[])
-    {
-        int n=a.length;
-        for(int x=0;x<n-1;x++)
-        {
-            for(int y=0;y<n-x-1;y++)
+   public static void bubbleSort(int a[])
+   {
+      int n = a.length;
+      for (int x = 0; x < n - 1; x++) 
+      {
+         for (int y = 0; y < n - x - 1; y++)
+         {
+            if (a[y] > a[y + 1])
             {
-                if(a[y]>a[y+1])
-                {
-                    int t=a[y];
-                    a[y]=a[y+1];
-                    a[y+1]=t;
-                }
+               int t = a[y];
+               a[y] = a[y + 1];
+               a[y + 1] = t;
             }
-        }
-        System.out.println("The sorted array is-\n");
-        for(int x=0;x<n;x++)
-        {
-            System.out.println(a[x]+" ");  
-        }
-        
-    }
-    public static void main(String[]args)
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of the array\n");
-        int n=sc.nextInt();
-        int a[]=new int[n];
-        for(int x=0;x<n;x++)
-        {
-            a[x]=sc.nextInt();
-        }
-        bubbleSort(a);
-        
-    }
+         }
+      }
+      System.out.println("The sorted array is-\n");
+      for (int x = 0; x < n; x++) 
+      {
+         System.out.println(a[x] + " ");
+      }
+
+   }
+   public static void main(String[] args) 
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the size of the array\n");
+      int n = sc.nextInt();
+      int a[] = new int[n];
+      for (int x = 0; x < n; x++) 
+      {
+         a[x] = sc.nextInt();
+      }
+      bubbleSort(a);
+
+   }
 }
