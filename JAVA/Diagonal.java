82ecf1ea5de83b68b01410f5b8b44919208129df
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2021-10-31 10:40:50 +0530

    formatted the code

--- JAVA/Diagonal.java
+++ JAVA/Diagonal.java
@@ -1,34 +1,32 @@
-//Sum of diagonal elements of matrix
-//Author-Ritika Rastogi
-//Date-31-10-2021
+/*
+  Sum of diagonal elements of matrix
+  Author-Ritika Rastogi
+  Date-31-10-2021
+*/
+
 import java.util.*;
-class Diagonal
-{
-    public static void main(String[]args)
-    {
-        Scanner sc=new Scanner(System.in);
+class Diagonal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows and columns in matrix");
-        int n=sc.nextInt();int sum=0;
-        int a[][]=new int[n][n];
-        for(int x=0;x<n;x++)
-        {
-            for(int y=0;y<n;y++)
-            {
-                a[x][y]=sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
+        int a[][] = new int[n][n];
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                a[x][y] = sc.nextInt();
             }
         }
         System.out.println("\n The matrix is-");
-        for(int x=0;x<n;x++)
-        {
-            for(int y=0;y<n;y++)
-            {
-               System.out.print(a[x][y]+" ");
-               if(x==y)
-               sum=sum+a[x][y];
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                System.out.print(a[x][y] + " ");
+                if (x == y)
+                    sum = sum + a[x][y];
             }
             System.out.println();
         }
-        System.out.println("The sum of diagonals of the matrix is-"+sum);
+        System.out.println("The sum of diagonals of the matrix is-" + sum);
 
     }
 }
